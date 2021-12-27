e5c75fbf105cc9416b52b52e1272c2bb9fb38c53
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-12-27 19:54:10 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import NavbarLogin from "./components/navbarlogin";
 
 function App() {
   return (
-    <NavbarLogin />
+    <>  
+      <Footer/>
+    </>
   );
 }
 
