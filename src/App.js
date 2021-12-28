f2fa2228ffb1ea79cdcc63a13ddb04c1b5b85e07
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-12-28 21:42:16 +0800

    add card psikolog & update footer font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import CardPsikolog from "./components/cardPsikolog";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import NavbarLogin from "./components/navbarlogin";
 
 function App() {
   return (
-    <>  
+    <> 
+      <Navbar/> 
+      <CardPsikolog/>
       <Footer/>
     </>
   );
--- src/components/styles/button.jsx
+++ src/components/styles/button.jsx
@@ -1,10 +1,12 @@
 export const Default = {
-    default : "btn capitalize text-base"
+    default : "btn capitalize"
   }
   
   export const ButtonType = {
     // untuk button login di navbar
-    login : "bg-transparent border-dark-green text-dark-green hover:btn-accent w-full lg:w-32", 
+    login : "bg-transparent border-dark-green text-dark-green hover:btn-accent w-full lg:w-32 text-base", 
     // untuk button signup di navbar
-    signUp : "bg-dark-green border-dark-green hover:btn-accent text-white w-full lg:w-32"
+    signUp : "bg-dark-green border-dark-green hover:btn-accent text-white w-full lg:w-32 text-base",
+    // button read more
+    more : "bg-dark-green border-dark-green hover:btn-accent text-white p-2 rounded-xl font-medium text-xs capitalize"
   }
