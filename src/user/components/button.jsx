87ed3f5626646c6b25e2abead9a41dc09e98dc34
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2022-01-11 14:40:39 +0800

    button

--- src/user/components/button.jsx
+++ src/user/components/button.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Default, ButtonType } from "./styles/button";
 import { Link } from "react-router-dom";
 
-export default function Button({def, type, children, onClick, href}) {
+export default function Button({def, type, children, onClick}) {
   const classNames = [ButtonType[type], Default[def]].join(" ");
   return (
-    <Link to={href}>
+    <>
       <button className= {classNames} onClick={onClick}>
       {children}
       </button>
-    </Link>
+    </>
   )
 }
