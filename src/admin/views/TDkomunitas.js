bd53c2f490948080c1c50856d0e4cbbd85432c36
Rianifitriim <59384629+Rianifitriim@users.noreply.github.com>
2022-01-14 10:45:47 +0700

    fixing admin

--- src/admin/views/TDkomunitas.js
+++ src/admin/views/TDkomunitas.js
@@ -94,12 +94,11 @@ export default function TDkomunitas() {
                         onChange={handleChange}></textarea>
             </div>
             <div className="flex flex-col mb-4">
-                <label className="text-left mb-2 font-bold text-lg text-gray-900" for="File">Gambar</label>
-                <input className="border py-2 px-3 text-black" type="file" name="file" id="file"
-                name="gambar" 
+                <label className="text-left mb-2 font-bold text-lg text-gray-900" for="first_name">Gambar</label>
+                <input className="border py-2 px-3 text-black" type="text" id="first_name"
+                name="judul" 
                 value={values.gambar}
-                onChange={handleChange}
-                />
+                onChange={handleChange}/>
             </div>
             <div className="flex flex-col mb-4">
                 <label className="text-left mb-2 font-bold text-lg text-gray-900" for="email">Judul Dokumentasi</label>
--- src/admin/views/profilPsikolog.js
+++ src/admin/views/profilPsikolog.js
@@ -64,12 +64,7 @@ export default function ProfilPsikolog() {
           </Link>
         </span>
                   </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-center text-xs font-medium text-dark-green uppercase tracking-wider"
-                  >
-                    Aksi
-                  </th>
+                  
                   <th
                     scope="col"
                     className="px-6 py-3 text-center text-xs font-medium text-dark-green uppercase tracking-wider"
@@ -107,27 +102,10 @@ export default function ProfilPsikolog() {
               {psikolog.map((psikolog)=>
               (
                   <tr>
-                    <td>
-                  <span className="hidden sm:block">
-          <button
-            type="button"
-            className="ml-2 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-white bg-blue-600" to="/admin/editdatapsikolog"
-          >
-            < span className="fas fa-edit -ml-1 mr-2 h-5 w-5 text-white" aria-hidden="true" />
-            Edit
-          </button>
-        </span>
-                  </td>
-                  <td>
-                  <span className="hidden sm:block">
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-white bg-red-500"
-          >
-            < span className="fas fa-trash-alt -ml-1 mr-2 h-5 w-5 text-white" aria-hidden="true" />
-            Hapus
-          </button>
-        </span>
+                   <td className="px-6 py-4 whitespace-nowrap">
+                    <span className="px-2 inline-flex text-xs leading-5 font-semibold text-black">
+                    
+                    </span>
                   </td>
                     <td className="px-6 py-4 whitespace-nowrap">
                     <span className="px-2 inline-flex text-xs leading-5 font-semibold text-black">
