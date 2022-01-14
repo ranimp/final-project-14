2a5dd3cdcc00803ba2a43eb3207cc294a6ca51b6
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2022-01-14 10:58:55 +0800

    fixing data profil

--- src/user/components/dataKonsultasi.jsx
+++ src/user/components/dataKonsultasi.jsx
@@ -1,15 +1,13 @@
-import { Link } from "react-router-dom"
-import { edit } from "../icons"
-import profil from "../images/profil.png"
+import profil from "../images/doctor.png"
 
-export default function DataKonsultasi() {
+export default function DataKonsultasi({name}) {
   return (
     <div className="bg-light-green font-poppins p-3 lg:p-12 rounded-lg">
       <div className="font-poppins text-black">
         <h1 className="text-dark-green text-center text-xl lg:text-3xl capitalize font-montserrat font-bold">konsultasi yang sedang diikuti </h1>
         <img src={profil} className="w-72 mx-auto my-4"/>
-        <p className="capitalize text-xs lg:text-base text-center">konsultasi masalah <span className="text-dark-green font-bold">anxiety</span></p>
-        <p className="capitalize text-xs lg:text-base text-center">Dr. Amanda Serena, M.Psi, Psikolog</p>
+        <p className="text-xs lg:text-base text-center">Konsultasi masalah <span className="text-dark-green font-bold">kesehatan mental </span>dengan</p>
+        <p className="capitalize text-xs lg:text-base text-center">{name}</p>
       </div>
     </div>
   )
--- src/user/components/dataProfil.jsx
+++ src/user/components/dataProfil.jsx
@@ -5,34 +5,34 @@ export default function ProfilData({name, email, email_ortu, birthdate, no_hp}) 
         <h1 className="text-dark-green text-xl lg:text-3xl capitalize font-montserrat font-bold">profil saya</h1>
       </div>
       <div className="grid grid-cols-7 text-black">
-        <div className="col-span-3 lg:col-span-2 capitalize text-xs lg:text-base">
+        <div className="col-span-2 lg:col-span-2 capitalize text-xs lg:text-base">
           <p>Nama</p>
         </div>
-        <div className="col-span-3 lg:col-span-5 text-xs lg:text-base">
+        <div className="col-span-5 lg:col-span-5 text-xs lg:text-base">
           <p>: {name}</p>
         </div>
-        <div className="col-span-3 lg:col-span-2 capitalize text-xs lg:text-base">
-          <p>Tanggal lahir</p>
+        <div className="col-span-2 lg:col-span-2 capitalize text-xs lg:text-base">
+          <p>Tanggal Lahir</p>
         </div>
-        <div className="col-span-3 lg:col-span-5 text-xs lg:text-base">
+        <div className="col-span-5 lg:col-span-5 text-xs lg:text-base">
           <p>: {birthdate.split('T').slice(0, 1).toString().split("-").reverse().join("-")}</p>
         </div>
-        <div className="col-span-3 lg:col-span-2 capitalize text-xs lg:text-base">
-          <p>Nomor telepon</p>
+        <div className="col-span-2 lg:col-span-2 capitalize text-xs lg:text-base">
+          <p>Telepon</p>
         </div>
-        <div className="col-span-3 lg:col-span-5 text-xs lg:text-base">
+        <div className="col-span-5 lg:col-span-5 text-xs lg:text-base">
           <p>: {no_hp}</p>
         </div>
-        <div className="col-span-3 lg:col-span-2 capitalize text-xs lg:text-base">
+        <div className="col-span-2 lg:col-span-2 capitalize text-xs lg:text-base">
           <p>Email</p>
         </div>
-        <div className="col-span-3 lg:col-span-5 text-xs lg:text-base">
+        <div className="col-span-5 lg:col-span-5 text-xs lg:text-base">
           <p>: {email}</p>
         </div>
-        <div className="col-span-3 lg:col-span-2 capitalize text-xs lg:text-base">
-          <p>Email Orang Tua</p>
+        <div className="col-span-2 lg:col-span-2 capitalize text-xs lg:text-base">
+          <p>Email ortu</p>
         </div>
-        <div className="col-span-3 lg:col-span-5 text-xs lg:text-base">
+        <div className="col-span-5 lg:col-span-5 text-xs lg:text-base">
           <p>: {email_ortu}</p>
         </div>
       </div>
